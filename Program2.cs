c522f07558b1e31d126df2579e91fb44104915f2
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2026-08-21 17:02:38 -0500

    rename program

--- Program2.cs
+++ Program2.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace snackShack
 {
-    internal static class Program
+    internal static class Program2
     {
         /// <summary>
         /// The main entry point for the application.
